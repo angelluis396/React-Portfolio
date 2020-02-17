02dc6e78a8811e878b8c64eae9a6c8f75251a582
Angel Alicea <ala31996@gmail.com>
2020-02-16 19:32:07 -0500

    added info to resume

--- src/components/experience.js
+++ src/components/experience.js
@@ -8,7 +8,7 @@ export default class Experience extends Component {
         <Grid>
           <Cell col={8}>
             <h4 style={{marginTop: '0px'}}> {this.props.jobName} </h4>
-            <p className="user-info"> {this.props.startYear} -h {this.props.endYear} </p>
+            <p className="user-info"> {this.props.startYear} - {this.props.endYear} </p>
             <p className="user-info"> {this.props.jobDescription} </p>
           </Cell>
           <Cell col={4}>
